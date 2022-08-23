a7fad67c1eba0a1b997bab2e669bb61966c632e9
Gustavo <gustavohenrique063@gmail.com>
2022-08-22 22:37:14 -0300

    cadastro_analista (dev)

--- lanternaverde_web/urls.py
+++ lanternaverde_web/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'), path("criar_analista", views.cadastro_analista, name= 'criar_analista')
 ]
--- lanternaverde_web/views.py
+++ lanternaverde_web/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Usuario, Analista, UserManager
+from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
+
+@csrf_exempt
+def cadastro_analista(request):
+    if request.method == 'GET':
+        pass
+
+    elif request.method == 'POST':
+        data = json.loads(request.body)
+        print(request.body)
+        print(data["username"])
+        usuario = UserManager().create_user(username=data["username"], email=data["email"], password=data["password"])
+        usuario.save()
+        analista = Analista.objects.create(cpf=data["cpf"],specialty=data["specialty"], user=usuario)
+        analista.save()
+
+
+    return HttpResponse(status=201)
+
+
+
+
+
+
+
+
+
+
+
+
