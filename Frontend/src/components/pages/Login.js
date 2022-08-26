9b004aa5324d0c59a13a530eaf7dcd2a7ea3a4e3
Julyanne Correia <julyanne.es@gmail.com>
2022-08-26 11:31:14 -0300

    website's Login popup

---
+++ Frontend/src/components/pages/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+import NavbarHome from '../NavbarHome';
+import Popup from '../Popup';
+
+function Login() {
+  return (
+    <>
+      <NavbarHome />
+      <Popup />
+    </>
+  );
+}
+
+export default Login;
