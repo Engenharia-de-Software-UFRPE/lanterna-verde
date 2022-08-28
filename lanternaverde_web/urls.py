3a7c0377c085c0e9dc12aa832379bc40608490aa
Sebastiao Vieira de Freitas Netto <sebastiao.fnetto@ufrpe.br>
2022-08-27 21:34:04 -0300

    correcao arquivo urls

--- lanternaverde_web/urls.py
+++ lanternaverde_web/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.login_redirect, name='login')
+    path('login', views.login_redirect, name='login'),
     path('cadastro_empresa', views.cadastro_empresa, name='cadastro_empresa'),
     path('alterar_empresa', views.cadastro_empresa, name='alterar_empresa')
 ]
