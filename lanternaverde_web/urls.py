4bf7b7f3aedc703bf8b1d1d3447b20a32f13e4ff
SteffanoP <steffanoxpereira@gmail.com>
2022-08-28 01:55:05 -0300

    fix/refactor: url `login` is no longer a redirect
    - removes HttpRedirect response for a HttpResponse
    - status codes are updated following RFC 7235
    - renames method to `login`

--- lanternaverde_web/urls.py
+++ lanternaverde_web/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.login_redirect, name='login'),
+    path('login', views.login, name='login'),
     path('user', views.get_logged_usuario, name='test_permission'),
     path('user/admin', views.get_logged_administrador, name='get_admin'),
     path('user/analista', views.get_logged_analista, name='get_anal'),
