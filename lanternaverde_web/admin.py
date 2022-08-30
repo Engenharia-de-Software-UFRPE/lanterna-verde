d6619aa0696a438e3ff4530ae3e278877379c9f0
Gustavo <gustavohenrique063@gmail.com>
2022-08-29 22:40:13 -0300

    models_questionario

--- lanternaverde_web/admin.py
+++ lanternaverde_web/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Questao, Usuario, Administrador, Analista
+from .models import Questao, Usuario, Administrador, Analista, AvaliacaoAnalista, Question
 
 # Register your models here.
 admin.site.register(Usuario)
 admin.site.register(Administrador)
 admin.site.register(Analista)
 admin.site.register(Questao)
+admin.site.register(AvaliacaoAnalista)
+admin.site.register(Question)
