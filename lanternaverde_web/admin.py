91eef22832a7c86b36c86f70d493b8220a71bc23
SteffanoP <steffanoxpereira@gmail.com>
2022-08-29 22:50:20 -0300

    refactor: renames `Questao` to `Pergunta`
    Renames `Questao` to `Pergunta` since `Questao` can be understood as question and answer and pergunta means only the question

--- lanternaverde_web/admin.py
+++ lanternaverde_web/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Questao, Usuario, Administrador, Analista
+from .models import Pergunta, Usuario, Administrador, Analista
 
 # Register your models here.
 admin.site.register(Usuario)
 admin.site.register(Administrador)
 admin.site.register(Analista)
-admin.site.register(Questao)
+admin.site.register(Pergunta)
