a0d7ec06d0c00c3c0a27680166c341fb3fb738b6
hagafedra <haga.fedra@gmail.com>
2022-08-31 20:25:56 -0300

    Popup implementado na Navbar

--- Frontend/src/components/HomeScreen/NavbarHome.js
+++ Frontend/src/components/HomeScreen/NavbarHome.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './NavbarHome.css';
+import './PopupLogin.css';
+
+import Popup from 'reactjs-popup';
 
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
@@ -39,7 +42,36 @@ function NavbarHome() {
                   <a class="nav-link" href="">Cadastro</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="Login">Login</a>
+                  <Popup trigger={<a class="nav-link" href="#">Login</a>}>
+                    <div className='popup'>  
+                      <Link
+                        to='/'
+                        >
+                        <Popup trigger={<a href="" id='x-btn'><strong>X</strong></a>}/>
+                      </Link>            
+                      <img src="../images/tick.png" alt="" />
+                      <h2>Faça seu login</h2>
+                      <p>Por favor digite seu login e senha.</p>
+                      <div>
+                        <input type="username" placeholder="Login" className="username" name ='username'/>
+                      </div>
+                      <div>
+                        <input type="password" placeholder="Senha" className="cPassword" />
+                      </div>
+                      <div id='buttons'>
+                        <ol>
+                          <div id='signin'>
+                            <li> <Link
+                              to='/Admin'
+                              >
+                              <a href="#">Entrar</a>
+                              </Link>
+                            </li>
+                          </div>
+                        </ol>
+                      </div>
+                    </div>
+                  </Popup>
                 </li>
               </ul> 
             </Navbar.Collapse>
