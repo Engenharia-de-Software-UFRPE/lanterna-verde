73b7702d1e187f0d6a1d4a6a5c188fc482b7eb8f
multi-megaman <evertonsilva2345meia78@gmail.com>
2022-09-09 22:36:07 -0300

    Tela para visualizar análises ja finalizadas implementada
    Algumas alterações foram feitas e agora já é possível acessar análises já finalizadas e ver quais respostas e qual comentário foi feito, porém não será possivel alterar os dados da página, servindo apenas para visualização

--- Frontend/src/components/Analyst/analysis/AnalysisDetails.js
+++ Frontend/src/components/Analyst/analysis/AnalysisDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Analysis.css'
 import Questions from './Questions';
+import QuestionsFinished from './QuestionsFinished';
 
 
 const Analysis = ({ analise }) => {
@@ -10,7 +11,12 @@ const Analysis = ({ analise }) => {
         console.log("Valor da chave 'comentário' da analise: " + analise.comentario);
     }
 
-    return <div className='listAnalise'>
+    const backButtonClickHandler =() => {
+
+    }
+
+    if (analise.finished === false){
+        return <div className='listAnalise'>
         Empresa: {analise.company}<br></br>
         {/* Questões: {analise.questoes} <br></br> */}
         Questões: {analise.questao_set.map((questao) => (<Questions questao={questao} />))}
@@ -20,6 +26,20 @@ const Analysis = ({ analise }) => {
         <button className='btn-save'>Salvar</button>
 
     </div>
+    }
+    else {
+        return <div className='listAnalise'>
+        Empresa que foi avaliada: {analise.company}<br></br>
+        {/* Questões: {analise.questoes} <br></br> */}
+        Respostas: {analise.questao_set.map((questao) => (<QuestionsFinished questao={questao} />))}
+        Score Atual: {analise.score}<br></br><br></br>
+        Comentário feito: <br></br>
+        <textarea readonly='true' id='comentField' className='comentArea' >{analise.comment}</textarea>
+        <button className='btn-back' onClick={backButtonClickHandler}>Voltar</button>
+
+    </div>
+    }
+
 }; 
 
 export default Analysis;
