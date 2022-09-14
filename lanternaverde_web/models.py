2d213ea6ab4a403f9f1b835a1f62888e914dbfb7
Ronaldo Rodrigues <ronaldo.rodriguesf@ufrpe.br>
2022-09-13 23:15:53 -0300

    Ok, ajustado
    Co-authored-by: Steffano Pereira <steffanoxpereira@gmail.com>

--- lanternaverde_web/models.py
+++ lanternaverde_web/models.py
@@ -1,5 +1,3 @@
-from email.policy import default
-from typing import List
 from django.core.mail import send_mail
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.core import validators
