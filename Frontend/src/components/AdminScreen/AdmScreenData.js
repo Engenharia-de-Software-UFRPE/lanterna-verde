b00a480244ec0f58e931bf5d8508770e58b3f6d9
Julyanne Correia <julyanne.es@gmail.com>
2022-09-14 01:24:56 -0300

    Dados do Adm no perfil

---
+++ Frontend/src/components/AdminScreen/AdmScreenData.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import axios from "axios";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+
+function AdmScreenData() {
+    const [user, setUser] = useState("placeholder");
+    const [administrator, setAdministrator] = useState("placeholder");
+
+    async function listAnalysis() {
+      axios.defaults.withCredentials = true;
+      let response = await axios
+        .get("http://localhost:8000/user/admin", { withCredentials: true })
+        .then((response) => response);
+      setAdministrator(response.data.Administrador);
+      setUser(response.data.Usuario);
+    }
+  
+    if (administrator === "placeholder") {
+      listAnalysis();
+    }
+    if (user === "placeholder") {
+      listAnalysis();
+    }
+  
+    async function handleButtonClick() {
+      console.log(user);
+    }
+    async function handleButtonClick2() {
+      console.log(administrator);
+    }
+  
+    console.log(user)
+    console.log(administrator)
+    return (
+      <>
+        <Container className="cont">
+          <Row class="row align-items-center">
+            <Col md="auto" className="col-edit">
+
+              <Row md="">
+                <Form.Group className="mb-3">
+                    <Form.Label>Nome Completo</Form.Label>
+                    <Form.Control size="lg" value= {`${user.first_name} ${user.last_name} `}   />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Nome de usuario</Form.Label>
+                    <Form.Control size="lg" value={user.username} />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>E-email</Form.Label>
+                    <Form.Control size="lg" value={user.email} />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Senha</Form.Label>
+                    <Form.Control size="lg" type="password" value="**********" />
+                </Form.Group>
+                {/* <Row class="row align-items-center">
+                    <Col md="auto">
+                    <a href="/dataAnalystEdit" class="contact-btn">
+                        Alterar Dados
+                    </a>
+                    </Col>
+                </Row> */}
+              </Row>
+            </Col>
+            </Row>
+        </Container>
+    </>
+  );
+}
+export default AdmScreenData;
