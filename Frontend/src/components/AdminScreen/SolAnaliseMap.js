264cf1cfdf72ca09a327cbd22014c08d5b37491d
Julyanne Correia <julyanne.es@gmail.com>
2022-09-19 21:51:04 -0300

    imports desnecessários

--- Frontend/src/components/AdminScreen/SolAnaliseMap.js
+++ Frontend/src/components/AdminScreen/SolAnaliseMap.js
@@ -1,6 +1,5 @@
 import SolAnalise from './SolAnalise';
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 const SolAnaliseMap =({soliciatoes_analise}) => {
     return(
