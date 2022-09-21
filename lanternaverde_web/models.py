cd46afb1b3d07d1f79f463d59a9e15ddea23d035
Lucas Henrique Gonzaga de Sales <71048167+hipera09@users.noreply.github.com>
2022-09-21 20:30:12 -0300

    fix: remove things
    Co-authored-by: Steffano Pereira <steffanoxpereira@gmail.com>

--- lanternaverde_web/models.py
+++ lanternaverde_web/models.py
@@ -1,5 +1,3 @@
-from email.policy import default
-from tkinter import CASCADE
 from django.core.mail import send_mail
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.core import validators
