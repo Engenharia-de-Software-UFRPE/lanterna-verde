11b339bc1d5632935553251b0dff7f62b7390b90
Igor <isidoroigor9@gmail.com>
2022-09-28 06:55:26 -0300

    Correção de conflitos com commit anterior

--- lanternaverde_web/urls.py
+++ lanternaverde_web/urls.py
@@ -39,10 +39,12 @@ urlpatterns = [
     path('empresa/assign-package', views.assinar_pacote, name='assinar_pacote'),
     path('empresa/analises', views.listar_analises_empresa, name='listar_analises_empresa'),
     path('empresa/analises/not-reanalyzed', views.listar_analises_passiveis_reanalise, name='listar_analises_empresa'),
-    path('empresa/analise/<str:pk>', views.get_solicitacao_empresa, name='get_analise_empresa'),
+    path('empresa/analise/<str:pk>', views.get_info_analise_empresa, name='get_info_analise_empresa'),
     path('empresa/analise/<str:pk>/solicitar-reanalise', views.solicitar_reanalise, name='solicitar_reanalise'),
     path('empresa/ranking', views.get_ranking, name='get_ranking'),
-    path('empresa/<str:id>', views.get_empresa, name='get_empresa'),
+    #path('empresa/<str:id>', views.get_empresa, name='get_empresa'),
+    path('empresa/report', views.compilar_relatorio_geral_empresa, name='compilar_relatorio_geral_empresa'),
+
 
     path('relatorio', views.get_relatorios, name='get_relatorios'),
     path('relatorio/comment', views.comment_relatorio, name='comment_relatorio'),
