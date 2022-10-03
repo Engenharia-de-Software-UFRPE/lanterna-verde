290d54d36fe97a660d0cea734badbd826133eb30
Julyanne Correia <julyanne.es@gmail.com>
2022-10-03 10:08:11 -0300

    fix: removido o null=True

--- lanternaverde_web/models.py
+++ lanternaverde_web/models.py
@@ -233,7 +233,7 @@ class AvaliacaoAnalista(models.Model):
     
     status = models.IntegerField(choices=STATUS_CHOICES, default=PENDING)
     
-    analysis_request = models.ForeignKey(SolicitacaoAnalise, on_delete=models.CASCADE, related_name='analises', null=True)
+    analysis_request = models.ForeignKey(SolicitacaoAnalise, on_delete=models.CASCADE, related_name='analises')
     update_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
