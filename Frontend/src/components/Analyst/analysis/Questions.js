6e4870ce0e82d7cfd10f9a99734e4d4a1f49d22a
multi-megaman <evertonsilva2345meia78@gmail.com>
2022-10-09 14:28:49 -0300

    Melhorias no corpo para a futura animação
    Agora foram colocados os elementos absolutos e relativos com relação ao corpo e aos botões para que a futura animação possa ser feita.

--- Frontend/src/components/Analyst/analysis/Questions.js
+++ Frontend/src/components/Analyst/analysis/Questions.js
@@ -39,18 +39,36 @@ const Questions = ({ questao, analysis, setDimensions, handleCheckBoxClick, hand
     return <>
             <div className='question-container'>
 
-            
+                {/* <div className='negative-container'> */}
+                    <button className='negative-btn'>X</button>
+                {/* </div> */}
+                
+
                 <div className='full-question-area'>
                     {/* <div className='question-and-awnser'> */}
-                    <button className='negative-btn'>X</button>
+                    
                     
                     {/* <div className='alig-areas'> */}
+
                         <div className='questionArea'>
-                            {questao.question.body} 
-                            </div>
+                        {questao.question.body} 
+                        
+                        </div>
+                        
+                        <br></br>
+                        <div className='justification-container'>
+                            Justificativa:<br></br> <textarea  className='justificationArea' id = {'questionJustification'+questao.id} onChange={justificationAreaChange}>{questao.justification}</textarea>  <br></br>
+                        </div>
+                        
+                        <div className='source-container'>
+                            Fonte:<br></br> <textarea  className='sourceArea' id = {'questionSource'+questao.id} onChange={sourceAreaChange}>{questao.source}</textarea> <br></br>
+                        </div>
+
+
+
                     {/* </div> */}
 
-                    <button className='positive-btn'>V</button>      
+                       
                             {/* <label class="switch">
                                 <input type="checkbox" onClickCapture={() => handleCheckBoxClick(questao)} defaultChecked={questao.answer}></input>
                                 <span class="slider round">
@@ -58,14 +76,10 @@ const Questions = ({ questao, analysis, setDimensions, handleCheckBoxClick, hand
                             </label>  */}
                     {/* </div> */}
                 </div>
+                {/* <div className='positive-container'> */}
+                    <button className='positive-btn'>V</button>   
+                {/* </div> */}
 
-                <div className='source-container'>
-                    Fonte:<br></br> <textarea  className='sourceArea' id = {'questionSource'+questao.id} onChange={sourceAreaChange}>{questao.source}</textarea> <br></br>
-                </div>
-
-                <div className='justification-container'>
-                    Justificativa:<br></br> <textarea  className='justificationArea' id = {'questionJustification'+questao.id} onChange={justificationAreaChange}>{questao.justification}</textarea>  <br></br>
-                </div>
 
             </div>
             </>
