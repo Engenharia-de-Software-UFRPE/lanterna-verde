bfea0bed8e9903e05ae90707ad6075fae6ebd03f
Ronaldo Rodrigues <ronaldo182019@gmail.com>
2022-10-26 15:38:26 -0300

    Correção na exceção

--- lanternaverde_web/control_avaliacaoAnalista.py
+++ lanternaverde_web/control_avaliacaoAnalista.py
@@ -199,9 +199,10 @@ def finalizar_analise(request):
                     analysis_request.save()
                     try:
                         control_relatorio.gerar_relatorio(analysis_request)
-                    except IntegrityError as dbe:
+                    except DatabaseError as dbe:
                         return HttpResponse(f"Sua análise foi finalizada, mas algo deu errado no lado do servidor. Em "
-                                            f"caso de divergências, entre em contato com o administrador. Motivo: {dbe}")
+                                            f"caso de divergências, entre em contato com o administrador. Motivo: {dbe}"
+                                            )
                 return HttpResponse("Sua análise foi finalizada com sucesso", status=200)
             return HttpResponse("Senha incorreta, a análise não foi finalizada", status=403)
         return HttpResponse("Você não é o responsável por esta análise", status=403)
