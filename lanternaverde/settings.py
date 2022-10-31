c12a5948834df9b700d6657a71cdda91019e24ad
Ronaldo Rodrigues <ronaldo182019@gmail.com>
2022-10-31 19:43:48 -0300

    allowed cors origin

--- lanternaverde/settings.py
+++ lanternaverde/settings.py
@@ -58,12 +58,9 @@ MIDDLEWARE = [
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-]
+CORS_ORIGIN_WHITELIST = ('http://localhost:3000',)
 
 ROOT_URLCONF = 'lanternaverde.urls'
 
